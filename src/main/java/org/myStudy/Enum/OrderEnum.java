a40871c31f6725dfb6cbc3f3a257270048b588bd
wzycumt <hdmwzy@sina.com>
2017-02-24 20:40:11 +0800

    update

---
+++ src/main/java/org/myStudy/Enum/OrderEnum.java
@@ -0,0 +1,31 @@
+package org.myStudy.Enum;
+
+public enum OrderEnum {
+	ASC(0, "升序"),
+	DESC(1, "降序");
+	
+	private int order;
+	private String orderDes;
+	
+	private OrderEnum(int order, String orderDes) {
+		this.order = order;
+		this.orderDes = orderDes;
+	}
+
+	public int getOrder() {
+		return order;
+	}
+
+	public String getOrderDes() {
+		return orderDes;
+	}
+
+	public static OrderEnum stateOf(int index) {
+		for (OrderEnum order : values()) {
+			if (order.getOrder() == index) {
+				return order;
+			}
+		}
+		return null;
+	}
+}
