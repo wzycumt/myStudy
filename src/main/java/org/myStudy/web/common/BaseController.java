ab058812073a1f1c938267ae4b95d0cfeebc633d
Administrator <Administrator@WZY-PC>
2017-09-23 21:31:11 +0800

    update

--- src/main/java/org/myStudy/web/common/BaseController.java
+++ src/main/java/org/myStudy/web/common/BaseController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.myStudy.dto.Query;
-import org.myStudy.dto.Query.SidePaginationEnum;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ui.Model;
